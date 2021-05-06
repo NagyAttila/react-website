c12866ed05d993cf2a4898b661af6a44c37d4a08
Attila <attila@example.com>
2021-05-06 21:47:00 +0200

    mini-fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
-REMOTE_DIR=react-build
+REMOTE_DIR=react-build/
 
 ssh_webpage.sh "mkdir -m 775 $REMOTE_DIR"
-rsync -ravz --exclude '.env'  --exclude '.git/' --exclude 'node_modules/' ./build webpage:$REMOTE_DIR
+rsync -ravz --exclude '.env'  --exclude '.git/' --exclude 'node_modules/' ./build/ webpage:$REMOTE_DIR
 ssh_webpage.sh "find $REMOTE_DIR -type d -exec sudo chmod 775 {} \;"
 ssh_webpage.sh "find $REMOTE_DIR -type f -exec sudo chmod 644 {} \;"
